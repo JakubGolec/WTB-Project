bd55890f5b88d4c5ac151b0be90bd5786440cc7c
Kasia <kz.muszynska@o2.pl>
2019-06-29 15:31:57 +0200

    package game version 1.0

--- src/main/java/com/WTBProject/WtbProjectApplication.java
+++ src/main/java/com/WTBProject/WtbProjectApplication.java
@@ -1,4 +1,4 @@
-package com.WTBProject.WTBProject;
+package com.WTBProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
